c9707b973bc398d6dfa2b58a71debf42514470e9
Gabriel Bena <gabriel.bena1@gmail.com>
2024-04-30 17:30:52 +0200

    submodules

---
+++ install.sh
@@ -0,0 +1,5 @@
+
+git submodule add https://github.com/GabrielBena/snntorch.git
+git submodule add https://github.com/GabrielBena/pytorch-eventprop.git
+git submodule add https://github.com/GabrielBena/yin_yang_data_set.git
+git submodule add https://github.com/GabrielBena/pytorch-meta.git
