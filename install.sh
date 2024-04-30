ddc7b06e2fbc7032e54c37495e602e793d930535
Gabriel <gabriel.bena1@gmail.com>
2024-04-30 18:22:49 +0200

    install script

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 
-git submodule add https://github.com/GabrielBena/snntorch.git
-git submodule add https://github.com/GabrielBena/pytorch-eventprop.git
-git submodule add https://github.com/GabrielBena/yin_yang_data_set.git
-git submodule add https://github.com/GabrielBena/pytorch-meta.git
+conda create -n cc_spinn python
+conda activate cc_spinn
+conda install pytorch torchvision torchaudio cpuonly -c pytorch
+pip install pyserial
+pip install matplotlib
+conda install ipywidgets
+conda install ipykernel
+pip install tqdm
+
+pip install -e snntorch/
+pip install -e pytorch-eventprop
+pip install -e yin_yang_data_set/ 
+pip install -e pytorch-meta/ 
+pip install -e py-spinnaker2/
